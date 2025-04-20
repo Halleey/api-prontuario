82deabce85f07c0b71d1b6285110ae06918628f1
Zard delas <aparecidohallyson@gmail.com>
2025-04-20 16:26:48 -0300

    clean

--- src/main/java/com/api/prontuario/dtos/ProntuarioRequestDTO.java
+++ src/main/java/com/api/prontuario/dtos/ProntuarioRequestDTO.java
@@ -1,6 +1,4 @@
 package com.api.prontuario.dtos;
-
-import java.time.LocalDateTime;
 import java.util.List;
 
 public class ProntuarioRequestDTO {
@@ -14,47 +12,23 @@ public class ProntuarioRequestDTO {
     public List<PrescriptionRequestDTO> getRequestDTOS() {
         return requestDTOS;
     }
-
-    public void setRequestDTOS(List<PrescriptionRequestDTO> requestDTOS) {
-        this.requestDTOS = requestDTOS;
-    }
-
-    public String doctorCrm() {
-        return doctorCrm;
-    }
-
-    public void setDoctorCrm(String doctorCrm) {
-        this.doctorCrm = doctorCrm;
-    }
-
     public String patientCpf() {
         return patientCpf;
     }
 
-    public void setPatientCpf(String patientCpf) {
-        this.patientCpf = patientCpf;
-    }
     public String diagnosis() {
         return diagnosis;
-    }
-
-    public void setDiagnosis(String diagnosis) {
-        this.diagnosis = diagnosis;
     }
 
     public String treatment() {
         return treatment;
     }
 
-    public void setTreatment(String treatment) {
-        this.treatment = treatment;
-    }
+
 
     public String observations() {
         return observations;
     }
 
-    public void setObservations(String observations) {
-        this.observations = observations;
-    }
+
 }
